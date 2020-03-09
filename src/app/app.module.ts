c6ef235de025f4f839337176be470a24500e8633
baxelson12 <bradaxelson@gmail.com>
2020-03-09 18:16:38 -0400

    Hookup moving parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// NGXS
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+
+import { environment } from '../environments/environment';
+
+// App
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppState } from './store/state/app.state';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    NgxsModule.forRoot([AppState]),
+    NgxsReduxDevtoolsPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

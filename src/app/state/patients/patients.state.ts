49acc556c13cdde7300283ff6b6492557fe52858
baxelson12 <bradaxelson@gmail.com>
2020-03-10 16:38:46 -0400

    Build out state

--- src/app/state/patients/patients.state.ts
+++ src/app/state/patients/patients.state.ts
@@ -1,14 +1,69 @@
-import { State, Action, StateContext } from '@ngxs/store';
+import { State, Action, StateContext, NgxsOnInit } from '@ngxs/store';
 import * as actions from './patients.actions';
+import { Patient } from 'src/app/models/patient';
+import { DataService } from 'src/app/data.service';
+import { tap } from 'rxjs/operators';
 
 export class PatientsStateModel {
-  public items: string[];
+  public loading: boolean;
+  public patients: Patient[];
+  public selected: Patient | {};
 }
 
 @State<PatientsStateModel>({
   name: 'patients',
   defaults: {
-    items: []
+    loading: false,
+    patients: [],
+    selected: {}
   }
 })
-export class PatientsState {}
+export class PatientsState {
+  constructor(private readonly ds: DataService) {}
+  ngxsOnInit({ dispatch }: StateContext<PatientsStateModel>) {
+    dispatch(new actions.AllPatients());
+  }
+
+  // All
+  @Action(actions.AllPatients)
+  all({ patchState, dispatch }: StateContext<PatientsStateModel>) {
+    patchState({ loading: true });
+    return this.ds
+      .all$<Patient>('patients')
+      .pipe(tap(p => dispatch(new actions.AllPatientsSuccess(p))));
+  }
+  @Action(actions.AllPatientsSuccess)
+  allSuccess(
+    { patchState }: StateContext<PatientsStateModel>,
+    { payload }: actions.AllPatientsSuccess
+  ) {
+    return patchState({ loading: false, patients: payload });
+  }
+
+  // Create
+  @Action(actions.CreatePatient)
+  create({ payload }: actions.CreatePatient) {
+    return this.ds.create$<Patient>('patients', payload);
+  }
+
+  // Read
+  @Action(actions.ReadPatient)
+  read(
+    { patchState }: StateContext<PatientsStateModel>,
+    { payload }: actions.ReadPatient
+  ) {
+    return patchState({ selected: payload });
+  }
+
+  // Update
+  @Action(actions.UpdatePatient)
+  update({ payload }: actions.UpdatePatient) {
+    return this.ds.update$<Patient>('patients', payload);
+  }
+
+  // Destroy
+  @Action(actions.DestroyPatient)
+  destroy({ payload }: actions.DestroyPatient) {
+    return this.ds.destroy$<Patient>('patients', payload);
+  }
+}

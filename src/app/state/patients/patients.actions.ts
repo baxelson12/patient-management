d8bef67be7a40faf26f2f1af2a2316c1fe2e0a6c
baxelson12 <bradaxelson@gmail.com>
2020-03-10 16:38:33 -0400

    Payloads should be public:)

--- src/app/state/patients/patients.actions.ts
+++ src/app/state/patients/patients.actions.ts
@@ -7,45 +7,45 @@ export class AllPatients {
 }
 export class AllPatientsSuccess {
   static readonly type = '[Patients API] Get all patients success';
-  constructor(payload: Patient[]) {}
+  constructor(public payload: Patient[]) {}
 }
 
 // Create
 export class CreatePatient {
   static readonly type = '[Patients API] Create patient';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 export class CreatePatientSuccess {
   static readonly type = '[Patients API] Create patient success';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 
 // Read
 export class ReadPatient {
   static readonly type = '[Patients API] Read patient';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 export class ReadPatientSuccess {
   static readonly type = '[Patients API] Read patient success';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 
 // Update
 export class UpdatePatient {
   static readonly type = '[Patients API] Update patient';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 export class UpdatePatientSuccess {
   static readonly type = '[Patients API] Update patient success';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 
 // Destroy
 export class DestroyPatient {
   static readonly type = '[Patients API] Destroy patient';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }
 export class DestroyPatientSuccess {
   static readonly type = '[Patients API] Destroy patient success';
-  constructor(payload: Patient) {}
+  constructor(public payload: Patient) {}
 }

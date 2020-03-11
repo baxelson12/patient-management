aca0e3bcb79dfb9bed9e8b2d6e6d2ec651d7a110
baxelson12 <bradaxelson@gmail.com>
2020-03-11 14:45:29 -0400

    Gut app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
-import { PatientsState } from './state/patients/patients.state';
-import { FormGroup, FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { Patient } from './models/patient';
-import * as actions from './state/patients/patients.actions';
 
 @Component({
   selector: 'app-root',
@@ -12,40 +6,5 @@ import * as actions from './state/patients/patients.actions';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  @Select(state => state.patients.patients) patients$: Observable<
-    Patient[]
-  >;
-
-  patient = new FormGroup({
-    name: new FormGroup({
-      first: new FormControl(),
-      middle: new FormControl(),
-      last: new FormControl()
-    }),
-    dob: new FormControl(),
-    email: new FormControl(),
-    phone: new FormControl(),
-    address: new FormGroup({
-      line_1: new FormControl(),
-      line_2: new FormControl(),
-      city: new FormControl(),
-      state: new FormControl(),
-      zip: new FormControl()
-    }),
-    gender: new FormControl(),
-    insurance: new FormControl(),
-    last_exam: new FormControl(),
-    notes: new FormControl()
-  });
-
-  constructor(private store: Store) {}
-
-  delete(patient: Patient) {
-    console.log(patient);
-    this.store.dispatch(new actions.DestroyPatient(patient));
-  }
-
-  submit() {
-    console.warn(this.patient.value);
-  }
+  constructor() {}
 }

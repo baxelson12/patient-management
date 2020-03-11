8ca41b3afeb7303c2da6511d4c353afab733b26a
baxelson12 <bradaxelson@gmail.com>
2020-03-11 10:01:54 -0400

    Add in dropdown to nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  dw = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }

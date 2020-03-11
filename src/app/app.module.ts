86c6f611484010326dab56729e1e469d20d41083
baxelson12 <bradaxelson@gmail.com>
2020-03-10 22:33:50 -0400

    Add in ngxs forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 // Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 // NGXS
 import { NgxsModule } from '@ngxs/store';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { PatientsState } from './state/patients/patients.state';
 
@@ -21,6 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    NgxsFormPluginModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
     NgxsModule.forRoot([PatientsState], {
       developmentMode: !environment.production

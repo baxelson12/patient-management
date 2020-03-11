98bc50deb559d1692753c0e89f2d0e14b5ea3bd4
baxelson12 <bradaxelson@gmail.com>
2020-03-10 21:22:52 -0400

    Selected should be partial

--- src/app/state/patients/patients.state.ts
+++ src/app/state/patients/patients.state.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class PatientsStateModel {
   public loading: boolean;
   public patients: Patient[];
-  public selected: Patient | {};
+  public selected: Partial<Patient>;
 }
 
 @State<PatientsStateModel>({

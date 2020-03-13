e9cc5b616671d7ca5edeeaee0be4f3aae26b67d6
baxelson12 <bradaxelson@gmail.com>
2020-03-13 08:09:12 -0400

    Dates should be stored a as string

--- src/app/models/patient.ts
+++ src/app/models/patient.ts
@@ -4,12 +4,12 @@ import { Name } from './name';
 export interface Patient {
   id: string;
   name: Name;
-  dob: Date;
+  dob: string;
   email?: string;
   phone: string;
   address: Address;
   gender: boolean;
   insurance?: string;
-  last_exam: Date;
+  last_exam: string;
   notes?: string;
 }

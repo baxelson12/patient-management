62fdc1a19d6d20391422f63d71e59ab4c21684e1
baxelson12 <bradaxelson@gmail.com>
2020-03-14 18:19:34 -0400

    fix: external js promise causing angular zone error

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, NgZone } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,14 @@ export class LoginComponent {
   constructor(
     private as: AuthService,
     private router: Router,
-    private zone: NgZone
+    private zone: NgZone,
+    private auth: AngularFireAuth
   ) {}
 
   login() {
-    this.as
-      .login()
-      .then(() =>
-        this.zone.run(() => this.router.navigate(['/patients']))
-      );
+    this.as.login();
+    this.auth.authState.subscribe(() =>
+      this.router.navigate(['/patients'])
+    );
   }
 }

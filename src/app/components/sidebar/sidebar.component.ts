8201e24a92c2c9f2bf9b9348e0675e2830039b67
baxelson12 <bradaxelson@gmail.com>
2020-03-18 17:58:06 -0400

    fix: form should reset when navigating

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { UpdateForm, UpdateFormValue } from '@ngxs/form-plugin';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  constructor(private store: Store) {}
+
+  formReset() {
+    this.store.dispatch(
+      new UpdateFormValue({ value: {}, path: 'patients.patient' })
+    );
+  }
+}
